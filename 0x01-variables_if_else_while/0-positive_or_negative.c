bf014c4b1cf3ac834c37f447d82bc19ef85f8839
Saad-Bouhayane <yoyosb007@gmail.com>
2023-03-17 17:12:20 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -27,36 +27,3 @@ int main(void)
 
 	return (0);
 }
-betty 0-positive_or_negative.c
-chmod u+x 0-positive_or_negative.c
-
-
-
-1-----vi 1-last_digit.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - print last digit in random number
- *
- * Description: print last digit in random number
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int n, last;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-	if (last > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else if (last == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last);
-	else if (last < 6 && last != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-	return (0);
-}
