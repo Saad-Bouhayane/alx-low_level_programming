181e1348659974dee61ef384ceffeadeb0b17685
Saad-Bouhayane <yoyosb007@gmail.com>
2023-04-17 22:46:17 -0700

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,12 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - prints the name of the file.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int s;
-
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
